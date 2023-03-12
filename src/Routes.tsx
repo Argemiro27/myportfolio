fe4cbb374bdef8a32a81cbbc1e634b7b4ead2325
Argemiro <argemirojunior123@gmail.com>
2023-03-12 17:57:50 -0300

    first commit

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route, BrowserRouter as Router, Navigate } from "react-router-dom";
 import { ContatoPage, InicioPage, ProjetosPage } from "./pages";
 
 const PageRoutes = () => {
-    return(
-      <Router>
-        <Routes>
-          <Route path="/inicio" element={<InicioPage />} />
-          <Route path="/projetos" element={<ProjetosPage />} />
-          <Route path="/contato" element={<ContatoPage />} />
-        </Routes>
-      </Router>
-    )
- }
- 
- export default PageRoutes;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/inicio" replace />} />
+        <Route path="/inicio" element={<InicioPage />} />
+        <Route path="/projetos" element={<ProjetosPage />} />
+        <Route path="/contato" element={<ContatoPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default PageRoutes;
